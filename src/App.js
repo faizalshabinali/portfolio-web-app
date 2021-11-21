921ff92904d2996b1e9593d8337632585fa43017
faizalshabinali <faizalshabinali@gmail.com>
2021-11-21 13:45:40 +0400

    All Pages Adedd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.scss';
 import NavBar from './Components/NavBar';
 import HomePage from './Pages/HomePage';
+import { Switch, Route } from 'react-router-dom';
+import AboutPage from './Pages/AboutPage';
+import PortfoliosPage from './Pages/PortfoliosPage';
+import BlogsPage from './Pages/BlogsPage';
+import ContactPage from './Pages/ContactPage';
 
 function App() {
   return (
@@ -10,7 +15,23 @@ function App() {
       </div>
       <div className='main-content'>
         <div className='content'>
-          <HomePage />
+          <Switch>
+            <Route exact path='/' exact>
+              <HomePage />
+            </Route>
+            <Route exact path='/about' exact>
+              <AboutPage />
+            </Route>
+            <Route exact path='/portfolios' exact>
+              <PortfoliosPage />
+            </Route>
+            <Route exact path='/blogs' exact>
+              <BlogsPage />
+            </Route>
+            <Route exact path='/contact' exact>
+              <ContactPage />
+            </Route>
+          </Switch>
         </div>
       </div>
     </div>
