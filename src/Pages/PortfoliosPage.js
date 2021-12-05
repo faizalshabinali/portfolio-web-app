20ca1bdd9908f6bba4870270bbe367ce35c52160
faizalshabinali <faizalshabinali@gmail.com>
2021-12-05 13:42:12 +0400

    portfolio button

--- src/Pages/PortfoliosPage.js
+++ src/Pages/PortfoliosPage.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import Title from '../Components/Title';
+import MenuItems from '../Components/MenuItems';
+import Categories from '../Components/Categories';
+import { useState } from 'react';
+import portfolios from '../Components/allportfolio';
 
+const allCategories = [
+  'All',
+  ...new Set(portfolios.map((item) => item.category)),
+];
 function PortfoliosPage() {
-  return <div className='PortfoliosPage'></div>;
+  const [categories, setCategories] = useState(allCategories);
+  const [menuItems, setMenuItems] = useState(portfolios);
+
+  const filter = (category) => {
+    if (category === 'All') {
+      setMenuItems(portfolios);
+      return;
+    }
+    const filteredData = portfolios.filter((item) => {
+      return item.category === category;
+    });
+    setMenuItems(filteredData);
+  };
+
+  return (
+    <div className='PortfoliosPage'>
+      <div className='portfolio-title'>
+        <Title title={'Portfolios'} span={'Portfolios'} />
+      </div>
+      <div className='portfolios-data'>
+        <Categories filter={filter} categories={categories} />
+        <MenuItems menuItem={menuItems} />
+      </div>
+    </div>
+  );
 }
 
 export default PortfoliosPage;
